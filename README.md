6cea7f005cfa487496ad6ddc9e7bce73c1a4dfd8
strongtiger-dev <benj.pdk@gmail.com>
2019-09-01 20:18:24 -0700

    add information about the Robinhood api

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Crypto HFT
+
+## Robinhood API Documentation:
+cURL to get a list of cryptocurrencies:
+```
+$ curl 'https://nummus.robinhood.com/currencies/' -H 'X-TimeZone-Id: America/Los_Angeles' -H 'Authorization: Bearer <REDACTED>' --compressed
+```
+
+cURL to get a list of currency pairs:
+```
+$ curl 'https://nummus.robinhood.com/currency_pairs/' -H 'X-TimeZone-Id: America/Los_Angeles' --compressed
+```
+
+cURL to get current market data for BTC in particular:
+```
+$ curl 'https://api.robinhood.com/marketdata/forex/quotes/3d961844-d360-45fc-989b-f6fca761d511/' -H 'Authorization: Bearer <REDACTED>' --compressed
+```
--- robinhood-scraper/main.py
+++ robinhood-scraper/main.py
@@ -1,37 +1,40 @@
 """
 Robinhood crypto market data scraper
-
-Example curl to get current market data for BTC:
-curl 'https://api.robinhood.com/marketdata/forex/quotes/3d961844-d360-45fc-989b-f6fca761d511/' -H 'Authorization: Bearer <REDACTED>' --compressed
 """
 import argparse
 import logging
+import time
 
 parser = argparse.ArgumentParser(description='Scrape robinhood data.')
 parser.add_argument('--token-file', type=str, required=True,
                     help='File that contains the auth access token to use')
 parser.add_argument('--output-csv', type=str, default="output.csv",
                     help='Output file to use. If the file exists, it will append onto it.')
-parser.add_argument('--sleep-time', type=int, default=60,
+parser.add_argument('--sleep-time', type=float, default=60.0,
                     help='Time to sleep between queries.')
 parser.add_argument('--log-level', type=str, choices=['INFO', 'DEBUG', 'WARNING'],
                     default='INFO', help='Log level.')
 
 def main(args):
     logging.info("Starting scraping with the following options:")
-    for arg_name in args:
-        logging.info("{0} = {1}".format(arg_name, args[arg_name]))
+    args_dict = vars(args)
+    for arg_name in args_dict:
+        logging.info("{0} = {1}".format(arg_name, args_dict[arg_name]))
 
     while True:    
         # Get token from file
-
+        with open(args.token_file, 'r') as tf:
+            token = tf.readline()
+        
         # Send request
+        resp = requests.get()
 
         # Save to file
 
         # Sleep 
+        time.sleep(args.sleep_time)
 
 if __name__ == "__main__":
     args = parser.parse_args()
     logging.basicConfig(level=args.log_level.upper())
-    main(vars(args))
+    main(args)
