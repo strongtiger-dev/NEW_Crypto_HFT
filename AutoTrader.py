490e48de30dbc72b4d5a614a7fc4ed962f0ef176
strongtiger-dev <benj.pdk@gmail.com>
2019-09-10 13:30:11 -0700

    Abstract data processing

--- AutoTrader.py
+++ AutoTrader.py
@@ -23,19 +23,23 @@ class AutoTrader:
         async for message in websocket:
             await websocket.send(message)
             data = list(map(float, message.split(' ')))
-            self.data_queue = np.append(self.data_queue, data)
-            if self.data_queue.shape[0] > 2000:
-                self.data_queue = self.data_queue[1:]
-                action = self.make_trade(self.data_queue)
+            process_data(data)
+
+    def process_data(self, data):
+        self.data_queue = np.append(self.data_queue, data)
+        if self.data_queue.shape[0] > 2000:
+            self.data_queue = self.data_queue[1:]
+            action = self.make_trade(self.data_queue)
+        print(self.data_queue)
 
     def make_trade(self, data):
         action, price = self.strategy.choose_action(data)
         if self.ORDER_TYPES[action] == "BUY":
             print("BUYING {} {}".format(self.symbol, self.quantity))
-            self.client.place_buy_order(self.symbol, self.quantity, price)
+            #self.client.place_buy_order(self.symbol, self.quantity, price)
         elif self.ORDER_TYPES[action] == "SELL":
             print("SELLING {} {}".format(self.quantity, self.symbol))
-            self.client.place_sell_order(self.symbol, self.quantity, price)
+            #self.client.place_sell_order(self.symbol, self.quantity, price)
         else: #Action == HOLD
             print("HOLD")
             pass
