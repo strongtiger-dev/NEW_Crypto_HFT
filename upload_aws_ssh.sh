652569b50f9c01ff0049c82a9357a650f560040f
strongtiger-dev <benj.pdk@gmail.com>
2020-01-16 12:51:01 -0800

    Remove unnecessary folders

--- upload_aws_ssh.sh
+++
@@ -1,9 +0,0 @@
-
-set -ex
-
-SCRAPER_IP="34.227.103.159"
-SCRAPER_PEM="~/saurav-mbp-09022019.pem"
-
-scp -i ${SCRAPER_PEM} robinhood-scraper/*.secret ec2-user@${SCRAPER_IP}:~
-scp -i ${SCRAPER_PEM} robinhood-scraper/*.py ec2-user@${SCRAPER_IP}:~
-ssh -i ${SCRAPER_PEM} ec2-user@${SCRAPER_IP}
